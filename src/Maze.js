cd960d33fee9b8402d0db796bf9826bcf8c9f356
Mikael Törnwall <mikael.tornwall@aalto.fi>
2019-06-29 16:18:26 +0100

    add basic game functions

--- src/Maze.js
+++ src/Maze.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import KeyboardEventHandler from 'react-keyboard-event-handler'
 
 const board = [
   ['x', 'x', 'x', 'x', ' ', 'x', 'x', 'x', 'x', 'x'],
@@ -17,34 +17,64 @@ const board = [
 
 class Maze extends React.Component {
 
-state = {
-  board: board
-}
+  state = {
+    board: board,
+    i: null,
+    j: null
+  }
+
+  componentDidMount() {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === 'o') {
+          this.setState({
+            i: i,
+            j: j
+          })
+        }
+      }
+    }
+  }
+
+  handleKeyPress = (key) => {
+    if (key === 'up') {
+      this.move(-1, 0)
+    } else if (key === 'right') {
+      this.move(0, 1)
+    } else if (key === 'down') {
+      this.move(1, 0)
+    } else if (key === 'left') {
+      this.move(0, -1)
+    }
+  }
+
+  isOk = (di, dj) => this.state.board[this.state.i + di][this.state.j + dj] === " " ? true : false
+
+  move = (di, dj) => {
+    if (this.isOk(di, dj)) {
+      let board = this.state.board
+      board[this.state.i][this.state.j] = " "
+      board[this.state.i + di][this.state.j + dj] = "o"
+      this.setState({
+        board: board,
+        i: this.state.i + di,
+        j: this.state.j + dj
+       })
+    }
+  }
 
   render() {
 
     const renderRow = (row, index) => row.map((col, j) => <td key={index + "," + j} id={index + "," + j}>{col}</td>)
     const renderBoard = () => this.state.board.map((row, index) => <tr key={index}>{renderRow(row, index)}</tr>)
 
-    const move = (event) => {
-      console.log(event.key)
-    }
-
-    const currentPosition = () => {
-      for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-          if (this.state.board[i][j] === 'o') {
-            return i + "," + j
-          }
-        }
-      }
-    }
-
     return (
       <table>
         <tbody>
           {renderBoard()}
-
+          <KeyboardEventHandler
+            handleKeys={['up', 'down', 'left', 'right']}
+            onKeyEvent={(key, e) => this.handleKeyPress(key)} />
         </tbody>
       </table>
     )
